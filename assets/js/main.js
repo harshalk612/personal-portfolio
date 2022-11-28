7acda53c0671ab3b4d2684e1becd5f9a49155cb5
harshalk612 <harshalkakaiya61@gmail.com>
2022-11-28 19:04:47 +0530

    Added another Social: Medium

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,26 +33,26 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SWIPER PROJECTS ===============*/
-// let swiperProjects = new Swiper(".projects__container", {
-//   loop: true,
-//   spaceBetween: 24,
+let swiperProjects = new Swiper(".projects__container", {
+  loop: true,
+  spaceBetween: 24,
 
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-//   mousewheel: false,
-//   keyboard: true,
-//   breakpoints: {
-//     1200: {
-//       slidesPerView: 2,
-//       spaceBetween: -56,
-//     },
-//   },
-// });
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: false,
+  keyboard: true,
+  breakpoints: {
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: -56,
+    },
+  },
+});
 
 // Will uncomment when more than 1 porject
 
